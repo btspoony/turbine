a29823fe02322052d460076c98566017036e1454
Tang Bo Hao <tech@btang.cn>
2023-10-26 15:11:24 +0800

    feat: add init world

---
+++ examples/gacha-simulator/bin/init-world.mjs
@@ -0,0 +1,10 @@
+import cp from "node:child_process";
+
+const worldName = "sample";
+const pubKey =
+  "fb62bbf229fb28f7d903334e99282699b06686b3bb6dab87fae3cef92acb17f43f576b332a2ee91c36f2b117d4264ba96c65eceac22bb92ab5a6aad24e94d7c0";
+
+const out = cp.execSync(
+  `flow transactions send ./transactions/platform/initialize-world.cdc ${worldName} ${pubKey} 10.0 --signer=default`
+);
+console.log("Result: ", out.toString());
