9d07f0f8afe179b584e4a0422657a0b172eaaa2b
Tang Bo Hao <tech@btang.cn>
2023-11-02 23:14:25 +0800

    feat: impl name panel

---
+++ examples/gacha-simulator-frontend/src/components/gacha/MainSection.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref, computed } from 'vue'
+// TODO
+</script>
+
+<template>
+<div class="flex items-center justify-center">
+  TODO
+</div>
+</template>
---
+++ examples/gacha-simulator-frontend/src/components/player/NamePanel.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { NAvatar, NInput, NButton, NDropdown } from 'naive-ui'
+import { useStorage } from '@vueuse/core'
+import md5 from 'js-md5'
+
+const userName = useStorage('x-app-username', '')
+const editModeName = ref('')
+
+const gravatarUrl = computed(() => {
+  if (typeof userName.value === 'string') {
+    const hash = md5(userName.value)
+    return `https://www.gravatar.com/avatar/${hash}?d=identicon`
+  } else {
+    return null
+  }
+})
+
+const dropdownOptions = ref([
+  {
+    label: 'Logout',
+    key: 'logout',
+  },
+])
+
+function handleDropdownCommand(command) {
+  if (command === 'logout') {
+    userName.value = null
+  }
+}
+
+function handleConfirmName() {
+  userName.value = editModeName.value;
+}
+</script>
+
+<template>
+<div class="flex items-center justify-between gap-2">
+  <template v-if="!userName">
+    <NInput v-model:value="editModeName" placeholder="Enter your name" round @keyup.enter="handleConfirmName" />
+    <NButton type="primary" strong round @click="handleConfirmName">Confirm</NButton>
+  </template>
+  <NDropdown v-else trigger="click" :options="dropdownOptions" show-arrow @select="handleDropdownCommand">
+    <div class="cursor-pointer flex items-center gap-2">
+      <n-avatar round size="medium" :src="gravatarUrl" />
+      <strong>{{ userName }}</strong>
+    </div>
+  </NDropdown>
+</div>
+</template>
