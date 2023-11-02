9bb116e7100542732e232233fbaef74ab81c09ec
Tang Bo Hao <tech@btang.cn>
2023-11-02 23:28:13 +0800

    feat: set client:only for namepanel

--- examples/gacha-simulator-frontend/src/components/gacha/MainSection.vue
+++ examples/gacha-simulator-frontend/src/components/gacha/MainSection.vue
@@ -1,12 +1,14 @@
-<script setup>
-import { ref, computed } from 'vue'
-// TODO
+<script setup lang="ts">
+// import { ref, computed } from 'vue'
+import { useStorage } from '@vueuse/core'
+
+const userName = useStorage('x-app-username', '')
 </script>
 
 <template>
 <main class="container m-a">
   <div class="flex items-center justify-center">
-    TODO
+    USER
   </div>
 </main>
 </template>
