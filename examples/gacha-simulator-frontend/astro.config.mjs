079dab8cbda6ea61c379ad221c91ff9430489ea8
Tang Bo Hao <tech@btang.cn>
2023-11-06 15:34:53 +0800

    fix: social image

--- examples/gacha-simulator-frontend/astro.config.mjs
+++ examples/gacha-simulator-frontend/astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gacha-demo.turbine.run",
   integrations: [
     vue({
       appEntrypoint: "/src/pages/_app",
